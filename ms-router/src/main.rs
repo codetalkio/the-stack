40b23c58e947392a3800be2bca21c88df11f77d2
Christian Kjær <ckl@codetalk.io>
2023-07-02 21:25:57 +0200

    Drop apollo-router dependency

--- ms-router/src/main.rs
+++ ms-router/src/main.rs
@@ -1,5 +1,5 @@
-use apollo_router::services::supergraph;
-use apollo_router::TestHarness;
+// use apollo_router::services::supergraph;
+// use apollo_router::TestHarness;
 use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use tower::ServiceExt;
 use tracing::{debug, info};
@@ -72,41 +72,46 @@ async fn handle_request(event: Request) -> Result<Response<Body>, Error> {
     // - CORS?
     // - HTTP compression
     let config = serde_json::json!({"supergraph": { "introspection": true }});
-    let router = TestHarness::builder()
-        .schema(include_str!("../supergraph.graphql"))
-        .with_subgraph_network_requests()
-        .configuration_json(config)?
-        .build_router()
-        .await?;
+    // let router = TestHarness::builder()
+        // .schema(include_str!("../supergraph.graphql"))
+        // .with_subgraph_network_requests()
+        // .configuration_json(config)?
+        // .build_router()
+        // .await?;
 
     // ...then create a GraphQL request...
-    let request = supergraph::Request::fake_builder()
-        .query(query)
-        .build()
-        .expect("expecting valid request");
+    // let request = supergraph::Request::fake_builder()
+        // .query(query)
+        // .build()
+        // .expect("expecting valid request");
 
     // ... and run it against the router service!
-    let r = router.clone();
-    let res = r
-        .oneshot(request.try_into()?)
-        .await?
-        .next_response()
-        .await
-        .expect("expecting a response from the router");
+    // let r = router.clone();
+    // let res = r
+        // .oneshot(request.try_into()?)
+        // .await?
+        // .next_response()
+        // .await
+        // .expect("expecting a response from the router");
 
     // Return the response.
-    let resp = match res {
-        Ok(response) => Response::builder()
+    // let resp = match res {
+        // Ok(response) => Response::builder()
+            // .status(200)
+            // .header("content-type", "application/json")
+            // .body(std::str::from_utf8(response.to_vec().as_slice())?.into())
+            // .map_err(Box::new)?,
+        // Err(err) => Response::builder()
+            // .status(400)
+            // .header("content-type", "application/json")
+            // .body(format!("{err}").as_str().into())
+            // .map_err(Box::new)?,
+    // };
+    let resp = Response::builder()
             .status(200)
             .header("content-type", "application/json")
-            .body(std::str::from_utf8(response.to_vec().as_slice())?.into())
-            .map_err(Box::new)?,
-        Err(err) => Response::builder()
-            .status(400)
-            .header("content-type", "application/json")
-            .body(format!("{err}").as_str().into())
-            .map_err(Box::new)?,
-    };
+            .body("".into())
+            .map_err(Box::new)?;
     Ok(resp)
 }
 
