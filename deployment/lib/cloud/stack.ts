33b493fbd8ff9fcbb5c4d27769a5c74612bd651f
Christian Kjær <ckl@codetalk.io>
2023-10-08 01:08:47 +0200

    Restructure deploy workflow

--- deployment/lib/cloud/stack.ts
+++ deployment/lib/cloud/stack.ts
@@ -2,13 +2,15 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as domain from "./domain";
 
-interface StackProps extends cdk.StackProps, domain.StackProps {}
+interface StackProps extends cdk.StackProps, domain.Props {}
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
     // Set up our domain stack.
-    new domain.Stack(this, "Domain", props);
+    new domain.Stack(this, "Domain", {
+      domain: props.domain,
+    });
   }
 }
