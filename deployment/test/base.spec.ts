44021b8184911e56e809cad7a1d7285b27cbb522
Christian Kjær <ckl@codetalk.io>
2023-10-07 15:50:28 +0200

    Set up deployment workflow

--- deployment/test/base.spec.ts
+++ deployment/test/base.spec.ts
@@ -1,26 +1,45 @@
+/**
+ * For more on testing, check out:
+ * - https://cdkworkshop.com/20-typescript/70-advanced-topics/100-construct-testing/1000-assertion-test.html
+ * - https://docs.aws.amazon.com/cdk/v2/guide/testing.html
+ */
 import { describe, expect, test } from "bun:test";
 import { Capture, Match, Template } from "aws-cdk-lib/assertions";
 import * as cdk from "aws-cdk-lib";
-import * as sns from "aws-cdk-lib/aws-sns";
 import { Stack as BaseStack } from "../lib/base/stack";
+import { Stack as DomainStack } from "../lib/base/domain";
 
 describe("Base Stack", () => {
   test("synthesizes the way we expect", () => {
+    // Set up environment variables.
+    const domainEnv = process.env.DOMAIN!;
+    expect(domainEnv).toBeDefined();
+
     const app = new cdk.App();
 
-    // Since the StateMachineStack consumes resources from a separate stack
-    // (cross-stack references), we create a stack for our SNS topics to live
-    // in here. These topics can then be passed to the StateMachineStack later,
-    // creating a cross-stack reference.
-    // const topicsStack = new cdk.Stack(app, "TopicsStack");
-
-    // Create the topic the stack we're testing will reference.
-    // const topics = [new sns.Topic(topicsStack, "Topic1", {})];
-
-    // Create the StateMachineStack.
-    const base = new BaseStack(app, "StateMachineStack", {});
+    // Create the base stack.
+    const baseStack = new BaseStack(app, "Base", {
+      domain: domainEnv,
+    });
+    const domainStack = new DomainStack(baseStack, "Base", {
+      domain: domainEnv,
+    });
 
     // Prepare the stack for assertions.
-    const template = Template.fromStack(base);
+    const templateDomain = Template.fromStack(domainStack);
+
+    // Assert that the stack creates a hosted zone.
+    templateDomain.hasResourceProperties("AWS::Route53::HostedZone", {
+      // The domain name is suffixed with a dot.
+      Name: `${domainEnv}.`,
+    });
+
+    // Assert that the stack creates a certificate.
+    templateDomain.hasResourceProperties(
+      "AWS::CertificateManager::Certificate",
+      {
+        DomainName: domainEnv,
+      }
+    );
   });
 });
