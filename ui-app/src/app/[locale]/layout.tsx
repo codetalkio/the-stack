e1a8a5029a2471eb889de72084cded28408fa34d
Christian Kjær <ckl@codetalk.io>
2024-01-10 06:46:58 +0100

    Change environment URL, conditionally add speed insights

--- ui-app/src/app/[locale]/layout.tsx
+++ ui-app/src/app/[locale]/layout.tsx
@@ -1,10 +1,9 @@
 import '../globals.css';
-// import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { unstable_setRequestLocale, getTranslations } from 'next-intl/server';
-// import { notFound } from 'next/navigation';
 import { ReactNode } from 'react';
 import { promises as fs } from 'fs';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,18 +16,6 @@ export async function generateStaticParams() {
   const localeFiles = await fs.readdir(`${process.cwd()}/messages`);
   return localeFiles.map((f) => ({ locale: f.replace(/\.json$/, '') }));
 }
-
-/**
- * Load the contents of a given locale's messages file.
- */
-// async function messagesContent(locale: string) {
-//   try {
-//     return (await import(`../../../messages/${locale}.json`)).default;
-//   } catch (error) {
-//     console.error('[messagesContent] Something went wrong importing the message content', error);
-//     notFound();
-//   }
-// }
 
 type Params = {
   params: Awaited<ReturnType<typeof generateStaticParams>>[0];
@@ -54,16 +41,13 @@ export default async function Layout({ children, params: { locale } }: Props) {
   // Required workaround for static rendering with next-intl.
   // https://next-intl-docs.vercel.app/docs/getting-started/app-router#static-rendering
   unstable_setRequestLocale(locale);
+
   return (
     <html lang={locale}>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {process.env.IS_VERCEL_BUILD ? <SpeedInsights /> : null}
+      </body>
     </html>
   );
-}
-
-{
-  /* import { NextIntlClientProvider } from 'next-intl';
-  <NextIntlClientProvider locale={locale} messages={messages}>
-  {children}
-</NextIntlClientProvider> */
 }
